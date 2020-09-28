811a689fe9dfddd43f04abe325fae397f3a48357
NX-first <58499064+NX-first@users.noreply.github.com>
2020-09-28 19:53:22 +0800

    例2-8

---
+++ university_afterworks/first_works/Count.java
@@ -0,0 +1,11 @@
+public class Count{
+	private int serialNumber;
+	private static int counter = 0;
+	public Count() {
+		counter++;
+		serialNumber = counter;
+	}
+	public static void main(String[] args){
+		System.out.println(counter);
+	}
+}
